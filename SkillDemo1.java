bdb6427814078a2edfc5ec9082d20e548cf635ba
qsewell <103156726+qsewell@users.noreply.github.com>
2022-04-26 17:52:20 -0700

    Created test for sumOfSquares; should fail

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,3 +1,7 @@
 public class SkillDemo1 {
 
+    static int sumOfSquares(int a, int b) {
+        return a*a + b*b;
+    }
+
 }
---
+++ SkillDemo1Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    
+    @Test
+    public void testSumofSquares() {
+        assertEquals(13, SkillDemo1.sumOfSquares(2, 2)); //Should fail
+    }
+}
